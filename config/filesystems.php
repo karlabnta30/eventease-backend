ec6ee0a9e274293769fcda415e67ab4fd7b1f184
Karl Abonita <abonitakarlsteven@gmail.com>
2026-09-22 21:58:08 +0800

    Add cloudinary disk definition to filesystems config

--- config/filesystems.php
+++ config/filesystems.php
@@ -24,7 +24,7 @@ return [
     | may even configure multiple disks for the same driver. Examples for
     | most supported storage drivers are configured here for reference.
     |
-    | Supported drivers: "local", "ftp", "sftp", "s3"
+    | Supported drivers: "local", "ftp", "sftp", "s3", "cloudinary"
     |
     */
 
@@ -58,6 +58,11 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
             'report' => false,
+        ],
+
+        // Added Cloudinary Driver Disk
+        'cloudinary' => [
+            'driver' => 'cloudinary',
         ],
 
     ],
