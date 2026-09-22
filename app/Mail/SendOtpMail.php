8b1f3b8efa43fdd6efd0719c989c0f69747b4f18
Karl Abonita <abonitakarlsteven@gmail.com>
2026-09-23 01:14:35 +0800

    Remove queue interface to ensure OTP emails send synchronously

--- app/Mail/SendOtpMail.php
+++ app/Mail/SendOtpMail.php
@@ -3,14 +3,12 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SendOtpMail extends Mailable implements ShouldQueue
+class SendOtpMail extends Mailable
 {
     use Queueable, SerializesModels;
 
