11882b690196d777c9b03c242102593c1f527d2e
Karl Abonita <abonitakarlsteven@gmail.com>
2026-09-22 22:04:24 +0800

    Add missing cloud array configuration to cloudinary config

---
+++ config/cloudinary.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the Laravel Cloudinary package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cloudinary Configuration
+    |--------------------------------------------------------------------------
+    |
+    | An HTTP or HTTPS URL to notify your application (a webhook) when the process of uploads, deletes, and any API
+    | that accepts notification_url has completed.
+    |
+    */
+    'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cloudinary Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your Cloudinary settings. Cloudinary is a cloud hosted
+    | media management service for all file uploads, storage, delivery and transformation needs.
+    |
+    */
+    'cloud_url' => env('CLOUDINARY_URL', 'cloudinary://914847219969642:YOUR_SECRET@ajkp9hbp'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cloud Array (Fixes the Undefined array key "cloud" error)
+    |--------------------------------------------------------------------------
+    */
+    'cloud' => [
+        'cloud_name' => 'ajkp9hbp',
+        'api_key'    => '914847219969642',
+        'api_secret' => env('CLOUDINARY_SECRET'),
+    ],
+
+    /**
+     * Upload Preset From Cloudinary Dashboard
+     */
+    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
+
+    /**
+     * Route to get cloud_image_url from Blade Upload Widget
+     */
+    'upload_route' => env('CLOUDINARY_UPLOAD_ROUTE'),
+
+    /**
+     * Controller action to get cloud_image_url from Blade Upload Widget
+     */
+    'upload_action' => env('CLOUDINARY_UPLOAD_ACTION'),
+];
