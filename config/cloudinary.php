0f9c91a957bd9f7f8b28c2a1413950ebbf6ff611
Karl Abonita <abonitakarlsteven@gmail.com>
2026-09-26 17:22:39 +0800

    Add root key and secret to cloudinary config to satisfy service provider

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -4,10 +4,26 @@ return [
 
     'cloud_url' => env('CLOUDINARY_URL'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Root-level keys (Fixes the Undefined array key "key" error)
+    |--------------------------------------------------------------------------
+    */
+    'key'        => env('CLOUDINARY_API_KEY'),
+    'secret'     => env('CLOUDINARY_API_SECRET'),
+    'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Nested cloud array
+    |--------------------------------------------------------------------------
+    */
     'cloud' => [
         'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
         'api_key'    => env('CLOUDINARY_API_KEY'),
         'api_secret' => env('CLOUDINARY_API_SECRET'),
+        'key'        => env('CLOUDINARY_API_KEY'),
+        'secret'     => env('CLOUDINARY_API_SECRET'),
     ],
 
     'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
